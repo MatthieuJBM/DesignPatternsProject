fea1cc3c2bc4426a886f344e19117fc8d5e7314c
Mateusz Baryla <m.baryla@protonmail.ch>
2025-07-04 16:38:00 +0200

    feat: Subcategory updated.

--- src/domain/Subcategory.java
+++ src/domain/Subcategory.java
@@ -2,9 +2,29 @@ package domain;
 
 import java.util.List;
 
-public class Subcategory extends Category {
+public class Subcategory {
+    private int id;
 
-    public Subcategory(int id, String color, String icon, String name, List<Integer> subcategoriesIds) {
-        super(id, color, icon, name, subcategoriesIds);
+    private String name;
+
+    public Subcategory(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
